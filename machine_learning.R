a67e68e0088a210880dc314f66e054269c688c2f
ThiagoLira <thlira15@gmail.com>
2016-06-03 17:29:27 -0300

    modified function for tests, NEURAL NET WORKS

--- machine_learning.R
+++ machine_learning.R
@@ -1,6 +1,11 @@
-# Machine learning
+library(ISLR)
 library(pracma)
 library(dplyr)
+library(jsonlite)
+library(reshape2)
+library(e1071)
+library(kknn)
+
 
 #t.test(College$PhD,College$Grad.Rate) para testar independencia
 
@@ -26,7 +31,6 @@ printSVMResults <- function(train,test,mylogit,kernelType){
 }
 
 
-
 #performs a variety of ML tests on the WIFI dataset
 #
 #Dataset MUST be in the following form, as outputed by function prepareData:
@@ -42,60 +46,10 @@ printSVMResults <- function(train,test,mylogit,kernelType){
 #
 #
 #
-
-tests <- function(data){
-  
-  tidyData <- data
-  
-  # Scaling data
-  maxs <- apply(dplyr::select(tidyData,-idZ), 2, max) 
-  mins <- apply(dplyr::select(tidyData,-idZ), 2, min)
-  scaled <- as.data.frame(scale(dplyr::select(tidyData,-idZ), center = mins, scale = maxs - mins))
-  #concatenate with response vector again 
-  scaled$idZ <- tidyData$idZ
-  
-  # Train-test random splitting for machine learning
-  # 30% for tests and the rest for training
-  
-  set.seed(1)
-  index <- sample(1:nrow(tidyData),round(0.7*nrow(tidyData)))
-  #Train and test UNESCALED
-  train <- tidyData[index,]
-  test <- tidyData[-index,]
-  #Train and test SCALED
-  train_s <- scaled[index,]
-  test_s <- scaled[-index,]
+tests <- function(train_s,test_s){
   
   
-  
-  #Train and test UNESCALED
-  #train<- dplyr::filter(tidyData,idZ==5 |idZ==4)
-  #test<- dplyr::filter(tidyData,idZ==6)
-  #test$idZ <- c(4,4,4,4,4)
-  
-  #train_s<- dplyr::filter(scaled,idZ==5 |idZ==4)
-  #test_s<- dplyr::filter(scaled,idZ==6)
-  #test_s$idZ <- c(4,4,4,4,4)
-  #Now, we will apply many diferent machine learning algorithms and see which yelds the best classification accuracy
-  
-  
-  
-  
-  #LOGISTIC REGRESSION 
-  
-  #number of classes
-  #labels <- 2
-  
-  
-  #trainX <- select(train,-idZ)
-  #testX <- select(test,-idZ)
-  #trainY <- select(train,idZ)
-  #testY <- select(test,idZ)
-  
-  #error <- oneVsAll(trainX,trainY,labels,testX,testY)
-  
-  
-  
+ 
   #NEURALNETWORK
   
   
@@ -117,8 +71,12 @@ tests <- function(data){
   n <- names(train_sn)
   f <- as.formula(paste("idZ ~", paste(n[!n %in% "idZ"], collapse = " + ")))
   
-  #nn <- neuralnet(f,data=as.matrix(train_sn),hidden=25,linear.output=FALSE,threshold=0.01)
+ train_sn<- train_sn + 1
   
+# nn <- neuralnet(f,data=as.matrix(train_sn),hidden=25,linear.output=FALSE,threshold=0.01)
+  
+ #assign("nn",nn,.GlobalEnv)
+ 
   # Visual plot of the model
   #plot(nn)
   
@@ -175,6 +133,7 @@ tests <- function(data){
   
   
 }
+
 
 
 
@@ -553,6 +512,173 @@ mapFeatures<- function(X1,X2){
       }
   
   return (out)
+}
+
+
+#
+#data will be parsed from http://server-api-wifi-indoor.herokuapp.com
+#Sample GET instructions can be found via that URL
+#
+#
+#GET the data from all the zones and points in a facility in database and return a raw matrix with measures
+#
+getMLdata <- function (facility,user_id){
+  
+  userid <- user_id
+  
+  #GET list of facilities on server
+  listaFacilities <- jsonlite::fromJSON(paste("http://server-api-wifi-indoor.herokuapp.com/facilities/user/",toString.default(userid),sep="")) 
+  
+  #GET id of desired facility
+  id <- dplyr::filter(listaFacilities, name == facility)$id
+  
+  
+  #Create url with id of desired facility
+  urlZones <- paste("http://server-api-wifi-indoor.herokuapp.com/zones/facility/",toString.default(id),sep="")
+  
+  
+  
+  #GET list of zones in facility
+  listZones <- jsonlite::fromJSON(urlZones)
+  
+  
+  
+  
+  zonesID <- listZones$id
+  
+  templistWifi <- NULL
+  listWifi <- NULL
+  measuresList <- NULL
+  listAP <- NULL
+  
+  #For each id, GET all the points
+  for (idZ in zonesID) {
+    #GET list of points in zone with id "id"
+    print(paste("Pegando pontos da zona",idZ))
+    urlPoints <- paste("http://server-api-wifi-indoor.herokuapp.com/points/zone/",toString.default(idZ),sep="") 
+    listPoints <- jsonlite::fromJSON(urlPoints)
+    pointsID <- listPoints$id
+    print(paste(" ", "Foi achado o ponto ",pointsID))
+    #empty WifiList for each zone before point loop
+    templistWifi <- NULL
+    #GET first measure in each point
+    for (idP in pointsID){
+      
+      #GET list of measures in point with id "idP"
+      print(paste("   ","Pegando measures do ponto: ",idP))
+      urlMeasures <- paste("http://server-api-wifi-indoor.herokuapp.com/measures/point/",toString.default(idP),sep="")
+      listMeasures <- jsonlite::fromJSON(urlMeasures)
+      
+      #GET FIRST MEASURE
+      urlAP<- paste("http://server-api-wifi-indoor.herokuapp.com/access_points/measure/",toString.default(listMeasures$id[1]),sep="")
+      
+      listAP <- jsonlite::fromJSON(urlAP)
+      
+      listAP <- dplyr::select(listAP,c(bssid,rssi))
+      
+      #FOR second measure to the last
+      for (mId in listMeasures$id[-1]){
+        urlAP<- paste("http://server-api-wifi-indoor.herokuapp.com/access_points/measure/",toString.default(mId),sep="")
+        listAPt <- jsonlite::fromJSON(urlAP)
+        #merge by BSSID, will end with dataframe with bssid and multiple rows of RSSIs
+        listAP <- merge(listAP,listAPt[c("bssid","rssi")],by="bssid")
+        
+      }
+      
+      #apply kalman filter for every measure
+      k<- matrix(nrow=nrow(listAP))
+      
+      for(i in 1:nrow(listAP)){
+        
+        temp <- listAP[i,-1]
+        
+        k[i,] <- kalmanFilter(temp)
+        
+      }
+      
+      
+      res <- cbind(listAP[,1],k)
+      
+      
+      print(paste("     ","Foi pega a measure: ",listMeasures$id[1]))
+      
+      listAccessPoints <- data.frame(bssid=res[,1],rssi=res[,2])
+      
+      #only bssi and rssi matter for the machine learning algorithm
+      
+      #create vector with bssid and rssi: each with the id of the zone and measure they are part of
+      
+      templistWifi <- cbind(t(cbind(dplyr::select(listAccessPoints,c(bssid,rssi)),idZ,listMeasures$id[1])),templistWifi)
+      
+      
+    }
+    
+    #create list across zones
+    listWifi <- cbind(listWifi,templistWifi)
+    
+  }
+  
+  return (listWifi)
+  
+}
+
+
+#convert dbM to Watts
+dbMtoWatt <- function (value){
+  
+  return (10^(value/10))
+}
+
+
+#transforms the data parsed in to the matrix in which the machine learning will be applied
+#Dataset will be outputed in the following form
+#
+#
+#
+#RSSID1 RSSID2 RSSID3 ...   idZ (id of Zone in which that measure was made)
+# -30     -39    -29         2
+# -20     -90    -20         3 
+#  ...
+#
+prepareData <- function (listWifi){
+  
+  listWifi <- listWifi
+  
+  #transpose matrix 
+  temp <- t(listWifi)
+  
+  #change matrix type to dataframe
+  dfWifi <- data.frame(temp)
+  
+  colnames(dfWifi)[4] <- "measureID"
+  
+  
+  #reshape dataframe for the machine learning algorithm
+  mlWifi <- reshape2::dcast(dfWifi, measureID + idZ ~ bssid, value.var = 'rssi')
+  
+  #remove measure ID
+  mlWifi[,1] <- NULL
+  
+  
+  #convert strings to numbers
+  mlWifi <- apply(mlWifi, 2, as.numeric)
+  
+  
+  
+  
+  #convert measures from dbM to Watts 
+  #TODO checar significancia dos algarismos na conversao
+  #mlWifi[,-1] <- apply(mlWifi[,-1],2,Vectorize(dbMtoWatt))
+  
+  #substitute NAs with zero
+  #we consider the signals which where not measured (i.e. the signal was too low) to be zero in power
+  mlWifi[is.na(mlWifi)] <- -120
+  
+  
+  #colnames(mlWifi) <- paste("t",colnames(mlWifi),"t",sep="")
+  
+  
+  return (as.data.frame(mlWifi))
 }
 
 
--- wifi.r
+++ wifi.r
@@ -7,207 +7,91 @@ library(e1071)
 library(kknn)
 
 
-#
-#data will be parsed from http://server-api-wifi-indoor.herokuapp.com
-#Sample GET instructions can be found via that URL
-#
-#
-#GET the data from all the zones and points in a facility in database and return a raw matrix with measures
-#
-getMLdata <- function (facility,user_id){
-
-userid <- user_id
-
-#GET list of facilities on server
-listaFacilities <- jsonlite::fromJSON(paste("http://server-api-wifi-indoor.herokuapp.com/facilities/user/",toString.default(userid),sep="")) 
-
-#GET id of desired facility
-id <- dplyr::filter(listaFacilities, name == facility)$id
-
-
-#Create url with id of desired facility
-urlZones <- paste("http://server-api-wifi-indoor.herokuapp.com/zones/facility/",toString.default(id),sep="")
-
-
-
-#GET list of zones in facility
-listZones <- jsonlite::fromJSON(urlZones)
-
-
-
-
-zonesID <- listZones$id
-
-templistWifi <- NULL
-listWifi <- NULL
-measuresList <- NULL
-listAP <- NULL
-
-#For each id, GET all the points
-for (idZ in zonesID) {
-      #GET list of points in zone with id "id"
-       print(paste("Pegando pontos da zona",idZ))
-       urlPoints <- paste("http://server-api-wifi-indoor.herokuapp.com/points/zone/",toString.default(idZ),sep="") 
-       listPoints <- jsonlite::fromJSON(urlPoints)
-       pointsID <- listPoints$id
-       print(paste(" ", "Foi achado o ponto ",pointsID))
-       #empty WifiList for each zone before point loop
-       templistWifi <- NULL
-       #GET first measure in each point
-       for (idP in pointsID){
-         
-         #GET list of measures in point with id "idP"
-         print(paste("   ","Pegando measures do ponto: ",idP))
-         urlMeasures <- paste("http://server-api-wifi-indoor.herokuapp.com/measures/point/",toString.default(idP),sep="")
-         listMeasures <- jsonlite::fromJSON(urlMeasures)
-         
-         #GET FIRST MEASURE
-         urlAP<- paste("http://server-api-wifi-indoor.herokuapp.com/access_points/measure/",toString.default(listMeasures$id[1]),sep="")
-
-         listAP <- jsonlite::fromJSON(urlAP)
-
-         listAP <- dplyr::select(listAP,c(bssid,rssi))
-         
-         #FOR second measure to the last
-         for (mId in listMeasures$id[-1]){
-           urlAP<- paste("http://server-api-wifi-indoor.herokuapp.com/access_points/measure/",toString.default(mId),sep="")
-           listAPt <- jsonlite::fromJSON(urlAP)
-           #merge by BSSID, will end with dataframe with bssid and multiple rows of RSSIs
-           listAP <- merge(listAP,listAPt[c("bssid","rssi")],by="bssid")
-
-         }
-         
-         #apply kalman filter for every measure
-         k<- matrix(nrow=nrow(listAP))
-         
-         for(i in 1:nrow(listAP)){
-
-           temp <- listAP[i,-1]
-      
-           k[i,] <- kalmanFilter(temp)
-           
-         }
-         
-         
-         res <- cbind(listAP[,1],k)
-         
-
-         print(paste("     ","Foi pega a measure: ",listMeasures$id[1]))
-     
-         listAccessPoints <- data.frame(bssid=res[,1],rssi=res[,2])
-         
-         #only bssi and rssi matter for the machine learning algorithm
-         
-         #create vector with bssid and rssi: each with the id of the zone and measure they are part of
-         
-         templistWifi <- cbind(t(cbind(dplyr::select(listAccessPoints,c(bssid,rssi)),idZ,listMeasures$id[1])),templistWifi)
-         
-         
-       }
-       
-       #create list across zones
-       listWifi <- cbind(listWifi,templistWifi)
-       
-}
-
-return (listWifi)
-
-}
-
-
-#convert dbM to Watts
-dbMtoWatt <- function (value){
-  
-  return (10^(value/10))
-}
-
-
-#transforms the data parsed in to the matrix in which the machine learning will be applied
-#Dataset will be outputed in the following form
-#
-#
-#
-#RSSID1 RSSID2 RSSID3 ...   idZ (id of Zone in which that measure was made)
-# -30     -39    -29         2
-# -20     -90    -20         3 
-#  ...
-#
-prepareData <- function (listWifi){
-  
-  listWifi <- listWifi
-  
-  #transpose matrix 
-  temp <- t(listWifi)
-  
-  #change matrix type to dataframe
-  dfWifi <- data.frame(temp)
-  
-  colnames(dfWifi)[4] <- "measureID"
-  
-  
-  #reshape dataframe for the machine learning algorithm
-  mlWifi <- reshape2::dcast(dfWifi, measureID + idZ ~ bssid, value.var = 'rssi')
-  
-  #remove measure ID
-  mlWifi[,1] <- NULL
-  
-  
-  #convert strings to numbers
-  mlWifi <- apply(mlWifi, 2, as.numeric)
-  
-  
-  
-  
-  #convert measures from dbM to Watts 
-  #TODO checar significancia dos algarismos na conversao
-  #mlWifi[,-1] <- apply(mlWifi[,-1],2,Vectorize(dbMtoWatt))
-  
-  #substitute NAs with zero
-  #we consider the signals which where not measured (i.e. the signal was too low) to be zero in power
-  mlWifi[is.na(mlWifi)] <- -120
-
-  
-  #colnames(mlWifi) <- paste("t",colnames(mlWifi),"t",sep="")
-
-  
-  return (as.data.frame(mlWifi))
-}
-
-#
-#
-#
-#
-#
-#
-#
-#
-#main script
-#
-#
-#
-#
-#
-#
-#
-
 
 
 
 
 #Name of facility
-facility <- "lira house"
+facility <- "scopuss"
 #user id
 user <- 2
 
-#rawData <- getMLdata(facility,user)
+#rawData1 <- getMLdata(facility,user)
 
-suppressWarnings( tidyData <- prepareData(rawData))
+#suppressWarnings( tidyData1 <- prepareData(rawData1))
 
 #zones id is a factor, not a number!
-tidyData$idZ <- as.factor(tidyData$idZ)
+#tidyData1$idZ <- as.factor(tidyData1$idZ)
+#rawData2 <- getMLdata("scopusadria",1)
+#tidyData3 <- prepareData(rawData2)
+#tidyData3$idZ <- c(9,9,9,9)
+
+
+
+#
+#
+#
+#    SEPARATE DATASET IN TRAIN AND TEST SETS
+#    THIS WILL BE DIFFERENT FOR EACH DATASET
+#
+#
+#
+#####################################################################################################
+
+#the following code removes entries with 90% of -120 measures, i.e. the signal was to weak to me measured
+bol <- tidyData1 == -120
+for (col in  2:ncol(bol)){
+ 
+  if( mean( bol[,col]) > .90){
+    tidyData1[col]<-NULL
+    
+  }
+  
+}
+
+
+tidyData2 <- merge(tidyData1,tidyData3,all=TRUE)
+
+tidyData2[is.na(tidyData2)] <- -120
+
+tidyData <- tidyData2
+
+
+# Scaling data
+maxs <- apply(dplyr::select(tidyData,-idZ), 2, max) 
+mins <- apply(dplyr::select(tidyData,-idZ), 2, min)
+scaled <- as.data.frame(scale(dplyr::select(tidyData,-idZ), center = mins, scale = maxs - mins))
+#concatenate with response vector again 
+scaled$idZ <- tidyData$idZ
+
+# Train-test random splitting for machine learning
+# 30% for tests and the rest for training
+
+set.seed(18687685)
+index <- sample(1:nrow(tidyData),round(0.7*nrow(tidyData)))
+#Train and test UNESCALED
+train <- tidyData[index,]
+test <- tidyData[-index,]
+#Train and test SCALED
+train_s <- scaled[index,]
+test_s <- scaled[-index,]
+
+
+
+##########################################################################################
+
+
+
+
+
+
+
 
 #tests galore!
-suppressWarnings(tests(tidyData))
+suppressWarnings(tests(train,test))
+
+
+
 
 
 
