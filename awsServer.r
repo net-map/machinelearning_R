d221e506250773ce2574485f5b4bff4d6fe95707
ThiagoLira <thlira15@gmail.com>
2016-08-08 23:25:26 -0300

    added path on server

--- awsServer.r
+++ awsServer.r
@@ -2,10 +2,13 @@ zones <- c(116,117,118,119,120,121,122,123,124,125,126,127,128,129)
 
 setwd("/home/ec2-user/machinelearning_R")
 
+source("serverFunctions.r")
 
 path <- "raw-data/trainingData.csv"
 
 datasets <- prepareUCIdata(path,zones)
 
+
+setwd("/home/ec2-user/machinelearning_R")
 
 saveRDS(datasets,"prepared-data/data1.rds")
