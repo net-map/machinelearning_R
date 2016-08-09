18124bff021320abee3f4447a23a9cf063f167fe
EC2 Default User <ec2-user@netmap.ddns.net>
2016-08-09 15:20:08 +0000

    changes

--- awsServer.r
+++ awsServer.r
@@ -1,12 +1,14 @@
 zones <- c(116,117,118,119,120,121,122,123,124,125,126,127,128,129)
 
-dirPath <- ("/home/ec2-user/machinelearning_R")
+dirPath <- ("/home/ec2-user/machinelearning_R/raw-data")
 
 source("serverFunctions.r")
 
-path <- "raw-data/trainingData.csv"
+dataPath <- "trainingData.csv"
 
-datasets <- prepareUCIdata(path,zones)
+trainPath <- file.path(dirPath,dataPath)
+
+datasets <- prepareUCIdata(trainPath,zones)
 
 
 setwd("/home/ec2-user/machinelearning_R")
