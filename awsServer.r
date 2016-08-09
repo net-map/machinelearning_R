3e23bf81b55e76cf93e67b66d9ab0adbb3f96af9
ThiagoLira <thlira15@gmail.com>
2016-08-09 12:22:51 -0300

    test

--- awsServer.r
+++ awsServer.r
@@ -1,12 +1,17 @@
-zones <- c(116,117,118,119,120,121,122,123,124,125,126,127,128,129)
-
-dirPath <- ("/home/ec2-user/machinelearning_R/raw-data")
-
 source("serverFunctions.r")
 
-dataPath <- "trainingData.csv"
+
+dirPath <- ("/home/ec2-user/machinelearning_R")
+
+
+
+dataPath <- "raw-data"
+
 
 trainPath <- file.path(dirPath,dataPath)
+
+
+zones <- c(116,117,118,119,120,121,122,123,124,125,126,127,128,129)
 
 datasets <- prepareUCIdata(trainPath,zones)
 
