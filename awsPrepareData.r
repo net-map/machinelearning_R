e057e5a2bdfa6f086c40830ab17cbf1139358c4a
EC2 Default User <ec2-user@netmap.ddns.net>
2016-08-09 15:44:44 +0000

    names

---
+++ awsPrepareData.r
@@ -0,0 +1,23 @@
+
+#recieve input zones from command line
+args = commandArgs(trailingOnly=TRUE)
+#default values
+if (length(args)==0){
+  args <- c("112","113","114")
+}
+#convert to numbers
+zones <- as.numeric(args)
+
+source("serverFunctions.r")
+
+dirPath <- ("/home/ec2-user/machinelearning_R")
+
+dataPath <- "raw-data"
+
+trainPath <- file.path(dirPath,dataPath)
+
+datasets <- prepareUCIdata(trainPath,zones)
+
+
+saveRDS(datasets,"prepared-data/UCIdata.rds")
+
