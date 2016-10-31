6f521ebe3b27c1333a7820452d196ad1239bfeb8
ThiagoLira <thlira15@gmail.com>
2016-10-31 16:13:34 -0200

    added somo comments and prints

--- serverFunctions.r
+++ serverFunctions.r
@@ -708,13 +708,14 @@ aws.SingleTest <- function (queueID,jsonMeasure=NULL,facilityID){
   print(str(trainedModels))
   
   
-  invisible(rJava::.jstrVal(trainedModels$Tree$classifier))
+  error1 <- try(rJava::.jstrVal(trainedModels$Tree$classifier))
   
   
-  #error <- try(invisible(rJava::.jstrVal(trainedModels$SMO$classifier)))
+  error <- try(invisible(rJava::.jstrVal(trainedModels$SMO$classifier)))
   
   #print(error)
-  
+  print(error1)
+  print(error)
   
   pathData <- paste("prepared-data/",facilityID,".rds",sep="")
   
